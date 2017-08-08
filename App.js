7b8d02f7e574083a87a1fb74458f4b9d6573b625
Mali Michael <malimccalla@gmail.com>
2017-08-08 11:29:27 +0100

    Adds custom animations initialization

--- App.js
+++ App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { TabNavigator, StackNavigator } from 'react-navigation';
 import { Provider } from 'react-redux';
+import * as Animatable from 'react-native-animatable';
+import animations from './animations';
 import MainNavigator from './navigation/MainNavigator';
 
 import store from './store';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    Animatable.initializeRegistryWithDefinitions(animations);
+  }
+
   render() {
     return (
       <Provider store={store}>
